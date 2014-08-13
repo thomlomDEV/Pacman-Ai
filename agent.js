1fa85c8d0a9c4b9cb167846c291433effd021204
Costa Michailidis <costa@innovationbound.com>
2014-08-12 20:11:06 -0400

    working on detecting surroundings

--- agent.js
+++ agent.js
@@ -10,55 +10,123 @@ var update;
 
     // The agent that will play pacman; hopefully well.
     var AGENT = {
-      state: {
+      status: {
         currentPosition: BOARD[11][8],
         goingTo: BOARD[11][7],
-        eaten: PACMAN.getUserState().eaten,
+        // eaten: PACMAN.getUserState().eaten,
+        ghosts: PACMAN.getGhosts(),
         lives: PACMAN.getUserState().lives,
-        score: PACMAN.getUserState().score,
-        ghosts: PACMAN.getGhosts()
+        score: PACMAN.getUserState().score
       },
+      beenTo: [
+        "11,8"
+      ],
+      state: [
+        null,
+        null,
+        'pellet',
+        'pellet'
+      ],
       snapshots: [
       ],
-      decideNextMove: function (state, options) {
+      move: function (status, options) {
+
+        // Check if you've been here already. If not, take note.
+        if (AGENT.beenTo.indexOf(status.currentPosition.id) === -1) {
+          AGENT.beenTo.push(status.currentPosition.id);
+        }
+
+        // Evaluate your surroundings.
+        var position = status.currentPosition.id.split(',');
+        position[0] = parseInt(position[0], 10);
+        position[1] = parseInt(position[1], 10);
+        var up = [ (position[0] - 1), position[1] ];
+        var down = [ (position[0] + 1), position[1] ];
+        var left = [ position[0], (position[1] - 1) ];
+        var right = [ position[0], (position[1] + 1) ];
+
+        AGENT.state = [
+          whatsOverHere(status, up),
+          whatsOverHere(status, down),
+          whatsOverHere(status, left),
+          whatsOverHere(status, right)
+        ];
+
+        // Check against snapshots.
+
+        // Crunch numbers and make a move.
         var random = Math.round(Math.random() * (options.length - 1) );
-        AGENT.move(options[random].dir);
+        AGENT.direct(options[random].dir);
         var position = options[random].id.split(',');
-        AGENT.state.goingTo = BOARD[position[0]][position[1]];
+        AGENT.status.goingTo = BOARD[position[0]][position[1]];
+
+        // How'd that turn out for ya? (use a callback)
+
+        // Store a new snapshot.
       },
-      move: function (direction) { // Moves pacman in whatever direction you'd like.
+      direct: function (direction) { // Moves pacman in whatever direction you'd like.
         if (direction === 'up') { PACMAN.up() }
         if (direction === 'down') { PACMAN.down() }
         if (direction === 'right') { PACMAN.right() }
         if (direction === 'left') { PACMAN.left() }
       },
       update: function (user, ghosts) { // Runs on every repaint. Checks to see if new board position is reached.
-        if (user.lives !== AGENT.state.lives) {
-          AGENT.state.goingTo = BOARD[11][7];
-          AGENT.state.lives -= 1;
+        if (user.lives !== AGENT.status.lives) {
+          AGENT.status.goingTo = BOARD[11][7];
+          AGENT.status.lives -= 1;
         }
-        if (user.position.x === AGENT.state.goingTo.x && user.position.y === AGENT.state.goingTo.y) {
+        if (user.position.x === AGENT.status.goingTo.x && user.position.y === AGENT.status.goingTo.y) {
           var userState = PACMAN.getUserState();
-          AGENT.state.currentPosition = AGENT.state.goingTo;
-          AGENT.state.eaten = userState.eaten;
-          AGENT.state.lives = userState.lives;
-          AGENT.state.score = userState.score;
-          AGENT.state.ghosts = PACMAN.getGhosts();
-          AGENT.decideNextMove(AGENT.state, AGENT.state.goingTo.go);
+          AGENT.status.currentPosition = AGENT.status.goingTo;
+          // AGENT.status.eaten = userState.eaten;
+          AGENT.status.lives = userState.lives;
+          AGENT.status.score = userState.score;
+          AGENT.status.ghosts = PACMAN.getGhosts();
+
+          // and.... move.
+          AGENT.move(AGENT.status, AGENT.status.goingTo.go);
         }
       }
     };
 
-    update = AGENT.update;
+    update = AGENT.update; // Export this for pacman.js to call.
 
     /*
      * Some Helper Functions:
      */
 
+    // What's over here, tells you what's in a particular box.
+    function whatsOverHere (status, place) {
+      if ( BOARD[place[0]] === undefined || !BOARD[place[0]][place[1]] ) { // First check for walls.
+        return console.log('Ooo, wall.');
+      }
+      var ghostPositions = status.ghosts.map(function (ghost) { // Then check for ghosts.
+        return [ghost.position.x, ghost.position.y];
+      });
+      console.log(ghostPositions, [BOARD[place[0]][place[1]].x, BOARD[place[0]][place[1]].y, ]);
+      if (false) { // Ahhh, ghost!
+        return console.log('Ghost!');
+      }
+      if (AGENT.beenTo.indexOf(place.toString()) !== -1) { // Well, if there's no ghosts, then maybe you've been here?
+        return console.log('been here');
+      } else { // Hmmm, ok, must be a pellet.
+        return console.log('must be a pellet');
+      }
+    }
+
+    // Quick way to check if arrays are equal.
+    function arraysEqual (arr1, arr2) {
+      if(arr1.length !== arr2.length) { return false; }
+      for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i]) { return false; }
+      }
+      return true;
+    }
+
     // Just hit spacebar to log some info.
     document.addEventListener('keydown', function (event) {
       if (event.keyCode === 32) {
-        console.log("Logging game state: ", AGENT.state);
+        console.log("Logging game status: ", AGENT);
       }
     }, false);
 
